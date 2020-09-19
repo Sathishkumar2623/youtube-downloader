d446971b5fcbff5ba309dbfe8aa3afea3f5cad44
Sathishkumar2623 <71538690+Sathishkumar2623@users.noreply.github.com>
2020-09-19 11:03:56 +0530

    Add files via upload

---
+++ youtube downloader.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+from pytube import Youtube
+
+def downloadVid():
+    global E1
+    string = E1.get()
+    yt=Youtube(str(string))
+    videos=yt.get_videos()
+    s=1
+    for v in videos:
+        print(str(s)+'.'+str(v))
+        s+=1
+    n=int(input("Enter your choice"))
+    vid=videos[n-1]
+    destination=str(input("Enter your destination"))
+    vid.download(destination)
+    print(yt.filename+"\n Has been downloaded")
+root=tk.Tk()
+
+
+w=tk.Label(root,text="Youtube downloader")
+w.pack()
+
+
+
+E1=tk.Entry(root,bd=5)
+E1.pack(side=tk.TOP)
+
+
+
+button= tk.Button(root,text="Download",fg="red",command=downloadVid)
+button.pack(side=tk.BOTTOM)
+
+root.mainloop()
